c026e845b3199478ab21118f8069b1530d22fbd4
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-01-22 12:03:27 -0500

    project list class implemented

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,55 @@
+//Project State Management
+
+class projectState {
+  private projects: any[] = [];
+}
+
 //validation
 interface Validatable {
   value: string | number;
-  required: boolean;
-  minLength: number;
-  maxLength: number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+
+  return isValid;
 }
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -18,7 +64,41 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   };
   return adjDescriptor;
 }
+//JS parses all the typescript before starting the DOM so it will be aware of all the classes, no mather its location
 
+class ProjectList {
+  element: HTMLElement;
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    ); //document fragment which its only children is the enclosing node that is inside of a template element.
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+//ProjectInput Class
 class ProjectInput {
   element: HTMLFormElement;
   templateElement: HTMLTemplateElement;
@@ -32,11 +112,12 @@ class ProjectInput {
       "project-input"
     )! as HTMLTemplateElement; //type casting
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
+    //this.templateElement.content refers to a document fragment that includes what is inside the template.
     const importedNode = document.importNode(
       this.templateElement.content,
       true //we add true here to indicate we want to copy even the dependants.
-    );
+    ); //importedNode is a document fragment.
+
     this.element = importedNode.firstElementChild as HTMLFormElement; //we are getting the form element here
     //this is the form element
     this.element.id = "user-input"; //we asociate the form element to an id so we can pass it properties from app.css
@@ -61,15 +142,29 @@ class ProjectInput {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
-    /* 
-    enteredTitle.trim().length === 0 ||
-    enteredDescription.trim().length === 0 ||
-    enteredPeople.trim().length === 0 */
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
 
     if (
-      validate({ value: enteredTitle, required: true, minLength: 5 }) &&
-      validate({ value: enteredDescription, required: true, minLength: 5 }) &&
-      validate({ value: enteredPeople, required: true, minLength: 5 })
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
       alert("invalid input");
       return;
@@ -116,3 +211,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
